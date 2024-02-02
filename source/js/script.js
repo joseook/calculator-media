f26adcc147299a7de069a6aced6cc98d98b5b1b5
José <126371426+joseook@users.noreply.github.com>
2024-02-01 22:58:34 -0300

    Update script.js

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,6 @@
 function calcularMedia() {
     const av1 = parseFloat(document.getElementById('av1').value);
     const av2 = parseFloat(document.getElementById('av2').value);
-    const av3 = parseFloat(document.getElementById('av3').value);
     const avt = parseFloat(document.getElementById('avt').value);
 
     if (isNaN(av1) || isNaN(av2) || isNaN(av3) || isNaN(avt)) {
@@ -9,14 +8,13 @@ function calcularMedia() {
         return;
     }
 
-    const media = ((av1 * 25) + (av2 * 25) + (av3 * 20) + (avt * 30)) / 100;
+    const media = ((av1 * 25) + (av2 * 25) + (avt * 50)) / 100;
     document.getElementById('resultado').innerText = `Sua média é: ${media.toFixed(2)}`;
 }
 
 function limparCampos() {
     document.getElementById('av1').value = '';
     document.getElementById('av2').value = '';
-    document.getElementById('av3').value = '';
     document.getElementById('avt').value = '';
     document.getElementById('resultado').innerText = '';
 }
